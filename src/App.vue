2bca131a0978087a8d8b794f41c47076859dc914
Cosimo <24959231+CosimoTiger@users.noreply.github.com>
2024-05-04 05:24:40 +0200

    Add page shape shadow drop, modularize styles

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,28 @@
 <script setup>
-  import { RouterLink, RouterView } from 'vue-router'
+  import { RouterView } from 'vue-router'
   import Logo from './components/icons/Logo.vue';
+  import CoolHeader from './components/CoolHeader.vue';
   import NavigationBar from './components/NavigationBar.vue';
 </script>
 
 <template>
-  <header>
-    <h1 id="title">marrowbuster</h1>
-  </header>
+  <CoolHeader></CoolHeader>
 
   <aside>
     <Logo width="100%"></Logo>
     <NavigationBar></NavigationBar>
   </aside>
 
-  <main>
-    <RouterView />
+  <main class="page-filter">
+    <div class="page-shape" id="main-page">
+      <RouterView />
+    </div>
   </main>
 </template>
 
-<style scoped>
-  @font-face {
-    font-family: 'Twitcher';
-    src: url(@/assets/fonts/twitcher.woff2) format('woff2');
-    font-display: swap;
-  }
-
-  #title {
-    text-shadow: 0 1px 0 #999, 0 2px 0 #999,
-      -1px 3px 0 #888, -1px 4px 0 #888,
-      -2px 5px 0 #777, -2px 6px 0 #777,
-      -3px 7px 0 #666, -3px 8px 0 #666,
-      -3px 9px 0 #555, -3px 10px 0 #555,
-      -4px 11px 0 #444, -4px 12px 0 #444,
-      -4px 10px 30px rgba(0, 0, 0, 0.5);
-    font-size: 84px;
-    margin: 0;
-    letter-spacing: 0.1em;
-    color: #EEE;
-    -webkit-text-stroke: 0.1px #222;
-  }
-
+<style>
   main {
     grid-area: main;
-    padding: 1.5rem 2.5rem;
-  }
-
-  /*
-   * Trimmed paper borders.
-   */
-  main,
-  nav {
-    border: var(--border-width) solid var(--border-color);
-
-    --border-width: 1px;
-    --trim-size: 50px;
-    --border-color: black;
-
-    clip-path: polygon(var(--trim-size) 0, 100% 0, 100% 100%, 0 100%, 0 var(--trim-size));
-    background: linear-gradient(135deg, var(--border-color) calc(0.707 * var(--trim-size) + var(--border-width)), #0000 0) border-box;
-    /* MUST be after! */
-    background-color: #FDFDFD;
-  }
-
-  /*
-   * Trim override.
-   */
-  nav {
-    --trim-size: 25px;
-
-    /* 
-     * Trim top-left and bottom-right if needed.
-     *
-    clip-path: polygon(var(--trim-size) 0,
-        100% 0,
-        100% calc(100% - var(--trim-size)),
-        calc(100% - var(--trim-size)) 100%,
-        0 100%,
-        0 var(--trim-size));
-     */
-    border-bottom-right-radius: 30px;
   }
 
   aside {
@@ -92,6 +35,32 @@
 
   header {
     grid-area: header;
+  }
+
+  /*
+   * Trimmed paper borders.
+   */
+  .page-shape {
+    --border-width: 1px;
+    --trim-size: 50px;
+    --border-color: black;
+
+    border: var(--border-width) solid var(--border-color);
+
+    clip-path: polygon(var(--trim-size) 0, 100% 0, 100% 100%, 0 100%, 0 var(--trim-size));
+    background: linear-gradient(135deg, var(--border-color) calc(0.707 * var(--trim-size) + var(--border-width)), #0000 0) border-box;
+    /* MUST come after! */
+    background-color: #FDFDFD;
+    border-bottom-right-radius: 30px;
+  }
+
+  .page-filter {
+    filter: drop-shadow(-3px 3px 2px rgba(105, 105, 105, 0.5));
+  }
+
+  #main-page {
+    padding: 1.5rem 2.5rem;
+    height: 100%;
   }
 
   @media (max-width: 992px) {
---
+++ src/components/CoolHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <h1 id="title">marrowbuster</h1>
+    </header>
+</template>
+
+<style scoped>
+    h1 {
+        text-shadow: 0 1px 0 #999, 0 2px 0 #999,
+            -1px 3px 0 #888, -1px 4px 0 #888,
+            -2px 5px 0 #777, -2px 6px 0 #777,
+            -3px 7px 0 #666, -3px 8px 0 #666,
+            -3px 9px 0 #555, -3px 10px 0 #555,
+            -4px 11px 0 #444, -4px 12px 0 #444,
+            -4px 10px 30px rgba(0, 0, 0, 0.5);
+        color: #EEE;
+        font-size: 84px;
+        letter-spacing: 0.1em;
+        -webkit-text-stroke: 0.1px #222;
+        margin: 0;
+    }
+</style>

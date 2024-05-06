11764a62638e92f055a2f628a580c5762aca40c1
Cosimo <24959231+CosimoTiger@users.noreply.github.com>
2024-05-07 01:51:21 +0200

    Make external links open in new tab

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,8 +1,8 @@
 <template>
     <h2>contact:</h2>
     <ul>
-        <li><a href="https://twitter.com/marrowbuster">twitter / 𝕏</a></li>
-        <li><a href="https://furaffinity.net/user/marrowbuster">furaffinity</a></li>
-        <li><a href="https://github.com/marrowbuster">github</a></li>
+        <li><a href="https://twitter.com/marrowbuster" target="_blank">twitter / 𝕏</a></li>
+        <li><a href="https://furaffinity.net/user/marrowbuster" target="_blank">furaffinity</a></li>
+        <li><a href="https://github.com/marrowbuster" target="_blank">github</a></li>
     </ul>
 </template>
